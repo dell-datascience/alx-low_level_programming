4ca42e8a0e074b4c35becc12defb40576f26d5d6
Albert <dell.datascience@gmail.com>
2023-02-17 09:56:44 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -print the alphabets to standard output
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		if (i != 133 || i != 101)
+			putchar(i);
+	putchar('\n')
+	return (0);
+}
