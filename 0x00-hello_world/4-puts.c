128b5c0c606430b2ff88ba72f3d816a9f8381afe
Albert <dell.datascience@gmail.com>
2023-02-16 18:01:43 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+
+/*
+ * This function puts out a sring
+ * And returns  0
+*/
+
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;	
+	return (0);	
 }
