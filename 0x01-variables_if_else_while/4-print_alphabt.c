39aa79dc82334502f5e6156b5fea8c3f4323c109
Albert <dell.datascience@gmail.com>
2023-02-17 10:21:56 -0800

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,10 @@ int main(void)
 	int i;
 
 	for (i = 97; i <= 122; i++)
-		if (i == 113)
-			break;
-		else if (i == 101)
-			break;
-		putchar(i);
+{
+		if (i != 101 && 1 != 113)
+			putchar(i);
+}
 	putchar('\n');
 	return (0);
 }
