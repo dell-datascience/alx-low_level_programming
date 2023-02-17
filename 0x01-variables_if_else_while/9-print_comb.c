48beb23c0556a186dd6e80c3847b9f408b86323d
Albert <dell.datascience@gmail.com>
2023-02-17 11:19:52 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all single numbers
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+		putchar(i);
+		putchar(', ');
+	putchar('\n');
+	return (0);
+}
