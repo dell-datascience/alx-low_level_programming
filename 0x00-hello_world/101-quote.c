74e05e975a17ac77e53d2962baa7b634a600ca58
Albert <dell.datascience@gmail.com>
2023-02-19 19:23:08 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,6 +12,6 @@ int main(void)
 	char message[] = "and that piece of art is useful\""
 		" - Dora Korpar, 2015-10-19\n";
 
-	write(STDOUT_FILENO, message, sizeof(message));
+	write(STDOUT_FILENO, message, 59);
 	return (1);
 }
