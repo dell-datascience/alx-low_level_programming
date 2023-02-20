0409801c6c775010725ae01120c38eb4a4eaebe7
Albert <dell.datascience@gmail.com>
2023-02-19 18:42:04 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,12 +9,7 @@
  */
 int main(void)
 {
-	char message1[] = "and that piece of art is useful\"";
-	char message2[] = "- Dora Korpar, 2015-10-19\n";
-	char message[80];
-
-	strcpy(message, message1);
-	strcat(message, message2);
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(STDOUT_FILENO, message, sizeof(message));
 	return (1);
