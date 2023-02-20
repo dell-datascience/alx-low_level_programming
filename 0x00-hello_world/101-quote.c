7200f59e464ae17ff08726c5b53dc88e2fc49301
Albert <dell.datascience@gmail.com>
2023-02-19 17:44:20 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  *
  * Return: Success (1)
  */
-int main(void)
+int main()
 {
-	const void* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19'\n";
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19'\n";
 
 		write(STDOUT_FILENO, message, sizeof(message));
 		return (1);
