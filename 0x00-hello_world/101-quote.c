528bc0be4fc6458f5b8706e97432513e98fed57c
Albert <dell.datascience@gmail.com>
2023-02-19 18:56:59 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char message[] = "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDOUT_FILENO, message, sizeof(message));
-	write(STDOUT_FILENO, "\n", sizeof("\n"));
+	write(STDOUT_FILENO, message, sizeof(message) - 1);
 	return (1);
 }
