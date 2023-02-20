acb133e8583a02954ac32a36a73a2d6a820fc54e
Albert <dell.datascience@gmail.com>
2023-02-20 07:33:09 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print combination of numbers
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+{
+		for (j = i + i; j <= 9; j++)
+{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8 || j != 89)
+{
+				putchar(',');
+				putchar(' ');
+}
+}
+}
+	putchar('\n');
+	return (0);
+}
