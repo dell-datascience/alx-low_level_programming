3ba0e50faceb9aab3b69495155b8a9dd8922eb34
Albert <dell.datascience@gmail.com>
2023-02-21 16:59:02 -0800

    solve 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+        print_alphabet_x10();
+        return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - prints alphabet
+ *
+ * Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char i;
+	char j;
+
+	for (j = 0; j <= 9; j++)
+{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+}
+}
