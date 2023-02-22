e7443c92bc9a7dcdcdaa4417a5fc019790e806bc
Albert <dell.datascience@gmail.com>
2023-02-21 21:16:05 -0800

    solve 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * print_last_digit - prints the sign of a number
+ *
+ * @n: number to which last digit is needed
+ *
+ * Return: return last digit of n
+ */
+int print_last_digit(int n)
+{
+	int result;
+	
+	if (n < 0)
+{
+		n *=-1;
+}
+	result = n%10;
+	_putchar(result + '0');
+	return (result);
+}
+
