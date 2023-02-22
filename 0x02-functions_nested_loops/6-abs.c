3de687bd8298a10b86063cf3b5a5d804e021426d
Albert <dell.datascience@gmail.com>
2023-02-21 20:51:27 -0800

    solve 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * _abs - prints the sign of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	int result;
+
+	result = abs(n);
+	return (result);
+}
+
