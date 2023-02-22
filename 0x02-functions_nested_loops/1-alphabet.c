686be2d4c8ecb2cbe7865ea88c12bf20e82afc46
Albert <dell.datascience@gmail.com>
2023-02-21 16:34:00 -0800

    solve 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: void
+*/
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'x'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+
