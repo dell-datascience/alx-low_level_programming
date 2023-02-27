f3783bdace3c7eab1aa870cdb8a307dd6c7184f3
Albert <dell.datascience@gmail.com>
2023-02-26 21:04:36 -0800

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+
+/*
+ * mul - multipies two integers
+ *
+ * @a: parameter one
+ * @b: parameter two
+ *
+ * Return: return the product of two parameters
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+
+	return (result);
+}
