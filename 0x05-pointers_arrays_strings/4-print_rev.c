913b043a72bf759803c057bc1ef10623ac1e9104
Albert <dell.datascience@gmail.com>
2023-02-27 20:59:49 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints string in revers
+ * @s: string parameter
+ *
+ */
+void print_rev(char *s)
+{
+	int n;
+
+	n = 0;
+
+	while (*(s + n) != '\0')
+	{
+		n++;
+	}
+	n -= 1;
+
+	while (n >= 0)
+	{
+		_putchar(s[n]);
+		n --;
+	}
+
+	_putchar('\n');
+
+}
