32f24911e83b6fd0b7ffaa9694b180b5d7d1c8b4
Albert <dell.datascience@gmail.com>
2023-02-27 16:21:38 -0800

    resolve void print_to_98 trailing spaces

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,24 +15,24 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i != n)	
-				printf(", ");
-			printf("%d", i);	
-		}
-	
+	for (i = n; i >= 98; i--)
+	{
+	if (i != n)
+	printf(", ");
+	printf("%d", i);
+	}
+
 	}
 	else
 	{
-		for (i = n; i <= 98; i++)
-		{
+	for (i = n; i <= 98; i++)
+	{
 
-			if (i != n )
-				printf(", ");
-		
-			printf("%d", i);
-		}
+	if (i != n)
+	printf(", ");
+
+	printf("%d", i);
+	}
 	}
 	putchar('\n');
 }
