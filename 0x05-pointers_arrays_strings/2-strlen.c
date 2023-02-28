506a02062c42979cf6350af192afff3d75e20374
Albert <dell.datascience@gmail.com>
2023-02-27 20:10:33 -0800

    resolve 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - calculates length of string
+ * @s: parameter one
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int result;
+
+	result = strlen(s);
+
+	return (result);
+}
