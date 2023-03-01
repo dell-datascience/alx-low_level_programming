3eb91cc002407a42e7614291a181cd25487e3950
Albert <dell.datascience@gmail.com>
2023-03-01 07:13:09 -0800

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 110 times the numbers from 0 to 9
+ *
+ * Return: always 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar('0' + (i / 10));
+			}
+			_putchar('0' + (i % 10));
+		}
+	
+		_putchar('\n');
+	}
+}
