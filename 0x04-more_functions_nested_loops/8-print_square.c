c710b8c1f8841af42b915818b896608e016b559e
Albert <dell.datascience@gmail.com>
2023-03-01 14:29:15 -0800

    8-print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - prints square followed by a new line
+ * @size: parameter
+ *
+ * Return: None
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar(35);
+		_putchar('\n');
+	}
+
+}
