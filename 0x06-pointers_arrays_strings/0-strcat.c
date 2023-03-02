218255dabda742ac5ae934115016cf1fe02e695e
Albert <dell.datascience@gmail.com>
2023-03-01 20:57:51 -0800

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - concatenate two strings
+ * @dest: parameter one
+ * @src: parameter two
+ *
+ * Return: return concatenated char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int dest_len;
+	int src_len;
+
+	dest_len = strlen(dest);
+	src_len = strlen(src);
+
+	for (i = 0; i < src_len && src[i] != '\0'; i++)
+	{
+		dest[dest_len + 1] = src[i];
+	}
+	dest[dest_len + src_len] = '\0';
+
+	return (dest);
+}
