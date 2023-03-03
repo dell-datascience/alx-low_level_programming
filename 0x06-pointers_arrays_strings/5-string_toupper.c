f573cc7cc1d2f4b83fdfe6aa5810dc5a22ea30fd
Albert <dell.datascience@gmail.com>
2023-03-02 21:07:19 -0800

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,8 @@
 char *string_toupper(char *s)
 {
 	int i;
-	int j;
 
-	j = strlen(s - 1);
-
-	for (i = 0; i < j && s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] > 'a' && s[i] < 'z')
 			s[i] -= 32;
