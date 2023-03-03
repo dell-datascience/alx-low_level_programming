f4eecae115d875bcc22c127fdedda763a390ffb7
Albert <dell.datascience@gmail.com>
2023-03-02 18:25:16 -0800

    fix 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
-		return (15);
-	else if (s1 < s2)
-		return (-15);
-	else
-		return (0);
+	int i;
+	int j;
 
+	i = 0;
+
+       while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		i++;
+	}
+       j = s1[i] - s2[i];
+
+	return (j);
 }
