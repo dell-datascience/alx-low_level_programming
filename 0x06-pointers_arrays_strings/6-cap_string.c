cb83594091443a47b99801b89209302171a8e773
Albert <dell.datascience@gmail.com>
2023-03-02 20:40:59 -0800

    solve 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * *cap_string - capitalize all words in string
+ * @s: parameter pointer
+ *
+ * Return: uppercase characters
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+	char delim[] = " \t\n,;.!?\"(){}";
+
+	j = strlen(s - 1);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] -= 32;
+				for (j = 0; delim[j] != '\0'; j++)
+					if (s[i] == delim[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
+
+	}
+	return (s);
+}
