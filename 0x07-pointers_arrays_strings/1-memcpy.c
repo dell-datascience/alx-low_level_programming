b2f3a54784f84fcd3943b43306cf3fb10139f337
Albert <dell.datascience@gmail.com>
2023-03-05 17:14:50 -0800

    updated 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,13 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	unsigned int len;
-
-	len = strlen(dest);
-
 	for (i = 0; i < n; i++)
 	{
-		dest[len + i] = src[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
