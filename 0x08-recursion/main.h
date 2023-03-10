fed9694add9e9f919fd9e65ad1e32407cd9a7552
Albert <dell.datascience@gmail.com>
2023-03-09 20:55:48 -0800

    solve 100 and update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,8 @@ int is_prime_number(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _strlen_recursion(char *s);
+int p1(char *s, int l);
+nt _sqrt_helper(int n, int low, int high);
 
 #endif
