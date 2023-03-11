415502bf4433c323619ac4c0710a8c9efbd8bbb6
Albert <dell.datascience@gmail.com>
2023-03-11 10:19:06 -0800

    update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,19 +12,20 @@ int main(int argc, char *argv[])
 {
 
 	int product;
-
-
+	int i;
 	
-	if ( argc - 1 == 2)
-	{
-		product = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d", product);
-	}
-	else
+	product = 1;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
+	for (i = 1; i < argc; i++)
+	{
+		product *= atoi(argv[i]);
+	}
+	printf("%d\n", product);
 	return (0);
 }
