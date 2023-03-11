eb240bddc76767302e77bdc1fed5cc8fff74310a
Albert <dell.datascience@gmail.com>
2023-03-10 20:58:46 -0800

    solve 1" git push

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to it
+ * @argc: argumanet count
+ * @argv: argument value
+ *
+ * Return: Success (0)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+	{
+		printf("%d\n", (argc - 1));
+	}
+	return (0);
+}
