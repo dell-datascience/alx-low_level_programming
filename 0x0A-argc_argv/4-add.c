3a703358c12587fb6defc6daddb3bee06a30496d
Albert <dell.datascience@gmail.com>
2023-03-11 19:29:34 -0800

    solve 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - adds two positive numbers
+ * @argc: counts of input
+ * @argv: value of inputs
+ *
+ * Return: Success (0)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+
+	}
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(*argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			sum += atoi(argv[i]);
+
+		}
+	printf("%d\n", sum);
+	}
+
+	return (0);
+}
