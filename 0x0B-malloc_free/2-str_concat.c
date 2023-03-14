23bcb56f4b46a8710630569316412161a3ba8e63
Albert <dell.datascience@gmail.com>
2023-03-14 15:24:16 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,15 +34,12 @@ char *str_concat(char *s1, char *s2)
 	char *array;
 	int i;
 	int j;
-	char empty[1];
-
-	empty[0] = '\0';
 
 	if (s1 == NULL)
-		*s1 = empty[0];
+		*s1 = "";
 
 	if (s2 == NULL)
-		*s2 = empty[0];
+		*s2 = "";
 
 	array = malloc(sizeof(char) * _strlen(s1) + sizeof(char) * _strlen(s2));
 
