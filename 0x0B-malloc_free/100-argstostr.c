aa81573e93547c5b6182f7fc60472a63de86c162
Albert <dell.datascience@gmail.com>
2023-03-15 09:17:36 -0700

    solve 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - length of string
+ * @s: string
+ *
+ * Return: integer value of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * argstostr - concatenates all arguments of program
+ * @ac: argument count
+ * @av: argumrnt value
+ *
+ * Return: point to a new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *result;
+	int i;
+	int j;
+	int total_size;
+
+	total_size = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		total_size += _strlen(*(av + i));
+	}
+
+	result = malloc(sizeof(char) * (total_size + ac + 1));
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(*(av + i)); i++)
+		{
+			*(result++) = *(*(av + i) + j);
+		}
+		*(result++) = '\n';
+	}
+	*(result - 1) = '\0';
+	return (result - total_size - ac);
+}
