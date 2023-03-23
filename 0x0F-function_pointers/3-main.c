d0003cc3fae56e89866771bfa41d461237c35ef5
Albert <dell.datascience@gmail.com>
2023-03-23 08:39:09 -0700

    solve 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of elements
+ *
+ * Return: Success (0)
+*/
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 4)
+	{
+	printf("Errormain\n");
+	exit(98);
+	}
+
+	result =  (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
+
+	return (0);
+}
