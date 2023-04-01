1ebc53914c24bd45ae9888661119744eefad3a7f
Albert <dell.datascience@gmail.com>
2023-03-31 21:35:20 -0700

    solve 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: singly linked list
+ *
+ * Return: number of node
+*/
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
+	n++;
+	}
+	return (n);
+}
