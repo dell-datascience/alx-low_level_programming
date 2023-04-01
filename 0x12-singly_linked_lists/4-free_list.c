1bfb6c3fa73c0bbbd016a77a9d9d9fcba7f011b2
Albert <dell.datascience@gmail.com>
2023-04-01 14:45:15 -0700

    solve  4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - free list_t
+ * @head: address of singly linked list
+*/
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+		head = head->next;
+	}
+}
