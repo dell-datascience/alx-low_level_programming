f590e6e5df8c38f22f4f85b6889683149e59ddc9
Albert <dell.datascience@gmail.com>
2023-03-31 22:31:18 -0700

    update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -32,7 +32,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-		return (NULL);i
+		return (NULL);
 
 	new->str = strdup(str);
 	new->len = len;
