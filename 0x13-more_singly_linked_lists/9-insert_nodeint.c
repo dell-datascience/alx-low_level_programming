76d65aed3f6e47297fd4aa454ebd77777b9ff405
Albert <dell.datascience@gmail.com>
2023-04-05 10:06:24 -0700

    update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		holder = *head;
 
-		while (i <  idx - 1 && holder != NULL)
+		while (i <  idx - 1)
 		{
 			holder = holder->next;
 			i++;
